efcdce8801c3207b6e438da859978fa23d6b972f
David Eisner <david.eisner@oriel.oxon.org>
2016-11-22 15:48:16 +0000

    Stave and 7/5 spacing

--- renderer.js
+++ renderer.js
@@ -8,15 +8,17 @@ var midi = new MIDIFile(fs.readFileSync("a.mid").buffer);
 
 var seq = midi.getMidiEvents().filter(x => x.subtype == 9 && x.track == 1);
 var pitches = seq.map(x => x.param1 + offset);
-var m = Math.min(...pitches), n = Math.max(...pitches);
+var low = Math.min(...pitches), high = Math.max(...pitches);
 var t = Math.min(...seq.map(x => x.playTime)) - 500;
 
 var scale = 20;
-var gap = 4;
+var gap = 1;
 var note = scale - gap * 2;
 
-var draw = SVG('drawing').size(30000, (n-m+12)*scale);
+var draw = SVG('drawing').size(30000, (high-low+12)*scale);
+var stave = draw.group();
 
+var degrees = [1, 1.5, 2, 2.5, 3, 4, 4.5, 5, 5.5, 6, 6.5, 7];
 var colours = ["red", "red", "blue", "blue", "saddlebrown", "lime", "lime", "yellow", "yellow", "darkviolet", "darkviolet", "grey"];
 var circle = draw.circle(note).move(-scale, -scale);
 circle.stroke({ color: "black", opacity: 0, width: 0 });
@@ -24,13 +26,32 @@ var square = draw.rect(note, note).move(-scale, scale);
 square.stroke("black");
 var shapes = [circle, square, circle, square, circle, circle, square, circle, square, circle, square, circle];
 
-for (var i = m - 11; i < n; i++)
-  if ((i % 12) == 0)
-    draw.line(0, (n-i+1)*scale-gap, 30000, (n-i+1)*scale-gap).stroke("black");
-
-seq.map(x =>
-  shapes[(x.param1+offset) % 12]
+for (var i = low - 11; i <= high; i++) {
+  var p = i;
+  var n = p % 12;
+  var h = (p - n)*7/12 + degrees[n] - 1;
+  if (degrees[i % 12] % 2 == 0 && i > low)
+    stave.line(0, (high*7/12-h)*scale+scale/2-gap, 30000, (high*7/12-h)*scale+scale/2-gap).stroke("grey").attr({"style": "z-index: 2"});
+  else if ((i % 12) == 0)
+    stave.line(0, (high-i)*7/12*scale+scale-gap, 30000, (high-i)*7/12*scale+scale-gap).stroke("black");
+}
+/*j=1;
+for (var i = low; i <= high; i++){
+  var p = i;
+  var n = p % 12;
+  var h = (p - n)*7/12 + degrees[n] - 1;
+  shapes[n]
     .clone()
-    .move((x.playTime - t)/900*scale, (n-x.param1-offset)*scale)
-    .fill(colours[(x.param1+offset) % 12])
-);
+    .move(j++*scale, (high*7/12-h)*scale)
+    .fill(colours[n])
+}
+return*/
+seq.map(x => {
+  var p = x.param1+offset;
+  var n = p % 12;
+  var h = (p - n)*7/12 + degrees[n] - 1;
+  shapes[n]
+    .clone()
+    .move((x.playTime - t)/900*scale, (high*7/12-h)*scale)
+    .fill(colours[n])
+});
