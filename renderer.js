1000f2ad730aa8d2ff9b528656d48d3b04341237
David Eisner <david.eisner@oriel.oxon.org>
2016-11-28 16:40:14 +0000

    stash

--- renderer.js
+++ renderer.js
@@ -2,6 +2,7 @@ var fs = require("fs");
 var MIDIFile = require("midifile");
 var MIDIPlayer = require("midiplayer");
 var SVG = require("svg.js");
+require("./svg.easing.js");
 
 const model = require('./model');
 
